d83d7d77bd33329ffa2a23ef4bfb523959d2e24b
TamuEX <c0117208c2@edu.teu.ac.jp>
2020-05-15 19:14:38 +0900

    [delete fix] posisonでの設定をflexでの設定に変更 class名の修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
   def top
-    @top3 = 1
     @shop = Shop.all.order(beer_price: "DESC")
     @beer_price = Shop.minimum(:beer_price)
+  end
+
+  def show
+    @shop = Shop.find_by(id: params[:id])
   end
 end
