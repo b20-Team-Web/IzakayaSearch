a0825ca4bd5772835a1761796a41c97cd517f893
TamuEX <c0117208c2@edu.teu.ac.jp>
2020-05-27 18:53:13 +0900

    [fix] application.jsに2つimport追加 ファイル構成変更 pack_tag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
+import 'bootstrap';
+import '../stylesheet/slick-theme.css';
+import '../stylesheet/slick.css';
+//https://stackoverflow.com/questions/60471982/when-to-use-stylesheet-pack-tag-instead-stylesheet-link-tag-with-rails-6
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
---
+++ app/javascript/packs/slick-custom.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(document).ready(function(){
+        console.log("こんばんわ");
+        $('.scroll_images').slick();
+    });
+})
