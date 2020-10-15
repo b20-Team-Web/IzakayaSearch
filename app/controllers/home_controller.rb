8cb2a126340654563e3c88b5e4789934f7bce1e9
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-10-16 00:17:28 +0900

    本番でエラーが出るため一旦コメントアウト

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
   before_action :check_user_agent_for_mobile
   
   def top
-    @shop = Shop.all.order(beer_price: "DESC")
-    @beer_price = Shop.minimum(:beer_price)
+    # @shop = Shop.all.order(beer_price: "DESC")
+    # @beer_price = Shop.minimum(:beer_price)
   end
 
   def show
-    @shop = Shop.find_by(id: params[:id])
+    # @shop = Shop.find_by(id: params[:id])
   end
 
   private
