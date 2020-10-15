78d81e22d22ed7da2bbd495b5a7de4196117260e
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-10-16 00:14:28 +0900

    migrate 重複　削除

--- db/migrate/20201009042926_create_drinks.rb
+++
@@ -1,9 +0,0 @@
-class CreateDrinks < ActiveRecord::Migration[6.0]
-  def change
-    create_table :drinks do |t|
-      t.string :name
-      t.integer :drink_type
-      t.timestamps
-    end
-  end
-end
