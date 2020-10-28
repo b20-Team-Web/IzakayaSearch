5c78eb3139f2e203ddf15c8fcee40a7d160755a2
TamuEX <c0117208c2@edu.teu.ac.jp>
2020-10-28 11:21:44 +0900

    [save] top.html.erbいじる前

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,17 +9,18 @@ class HomeController < ApplicationController
     sql = "select store_code, name from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id where drink_prices.id in(select min(drink_prices.id) from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id group by store_code, drinks.name);"
     @drink_name = ActiveRecord::Base.connection.select_all(sql).to_hash
 
-    sql = "select drink_prices.store_code, stores.name as store_name, image1, access_station_exit, access_walk, pr, budget, drink_price, drinks.name as drink_name from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id INNER JOIN stores ON drink_prices.store_code = stores.code where drink_prices.id in(select min(drink_prices.id) from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id group by store_code, drinks.name);"
+    sql = "select drink_prices.store_code, stores.name as store_name, image1, access_station_exit, access_walk, pr, budget, drink_price, drinks.name as drink_name from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id INNER JOIN stores ON drink_prices.store_code = stores.code where drink_prices.id in(select min(drink_prices.id) from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id group by store_code, drinks.name) ORDER BY drink_price ASC;"
     @store = ActiveRecord::Base.connection.select_all(sql).to_hash
 
     #@h = [{"first" => "a", "name" => ["b","c","d"]}, {"first" => "b", "name" => ["e","f","g"]}]
     
-    @drink_data = @drink_name.group_by{|h| h["store_code"]}.map{|k,a| [k,a.map{|h| h["name"]}]}.to_h
+    #@drink_data = @drink_name.group_by{|h| h["store_code"]}.map{|k,a| [k,a.map{|h| h["name"]}]}.to_h
   
     #@stores_data = @store.group_by{|h| h["store_code"]}.map{|n,a| [n,a.map{|h| h["name"]}]}
 
-    @stores_data = @store.group_by{|s| s["store_code"]}.map{|k,ary| a0=ary.first.dup;a0["drink_name"] = [];a0["drink_price"]=ary.map{|a| a["drink_price"]}.min; ary.inject(a0){|a,s| a["drink_name"]= [a["drink_name"], s["drink_name"]].flatten;a}}
-
+    #@stores_data = @store.group_by{|s| s["store_code"]}.map{|k,ary| a0=ary.first.dup;a0["drink_name"] = [];a0["drink_price"]=ary.map{|a| a["drink_price"]}.min; ary.inject(a0){|a,s| a["drink_name"]=[a["drink_name"], s["drink_name"]].flatten;a}}
+    
+    @stores_data = @store.group_by{|s|s["store_code"]}.map{|k,ary|a0=ary.first.dup;a0["drink_name"]=[]; a0["drink_price"]=ary.map{|a| a["drink_price"]}.min; ary.inject(a0){|a,s| a["drink_name"] << s["drink_name"];a}}
   end
 
   def show
