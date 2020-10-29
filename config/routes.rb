690afe3e5d21c00210168b3d9b26f84482b13dd0
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-10-29 10:40:39 +0900

    [fix] SPデザイン回収

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/' => 'home#top'
+  get '/'         => 'home#top'
+  get '/:params'  => 'home#top'
   get '/show/:id' => 'home#show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/gurunabi' => 'gurunabi#index'
 end
