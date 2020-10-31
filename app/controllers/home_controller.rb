01e68dc918e33da69a26ca81d53d74060af92e33
jotasugiyama <c0117165b3@edu.teu.ac.jp>
2020-10-31 09:13:46 +0900

    [fix] card のcss修正, 平均価格->平均予算へ

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,7 +19,7 @@ class HomeController < ApplicationController
       sql = "select drink_prices.store_code, stores.name as store_name, image1, access_line, access_station, access_station_exit, access_walk, pr, budget, drink_price, drinks.name as drink_name from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id INNER JOIN stores ON drink_prices.store_code = stores.code where drink_prices.id in(select min(drink_prices.id) from drink_prices INNER JOIN drinks ON drink_prices.drink_id = drinks.id group by store_code, drinks.name) ORDER BY budget, drink_price;"
       @store = ActiveRecord::Base.connection.select_all(sql).to_hash
       @stores_data = @store.group_by{|s|s["store_code"]}.map{|k,ary|a0=ary.first.dup;a0["drink_name"]=[]; a0["drink_price"]=ary.map{|a| a["drink_price"]}.min; ary.inject(a0){|a,s| a["drink_name"] << s["drink_name"];a}}
-      @h2_title = "平均価格が安い順"
+      @h2_title = "平均予算が安い順"
     end
   end
 
